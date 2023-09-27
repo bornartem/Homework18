b030a523b2c9cbf3d123bb738a230ccb93747a43
bornartem <born.artem@gmail.com>
2023-09-27 15:50:37 +0300

    1 commmit

---
+++ src/main/java/study/java/springfirstlesson/SpringFirstLessonApplication.java
@@ -0,0 +1,13 @@
+package study.java.springfirstlesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFirstLessonApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringFirstLessonApplication.class, args);
+    }
+
+}
