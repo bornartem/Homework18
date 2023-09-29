21c1f32aef35d1183794355a12a8002b5f118051
bornartem <born.artem@gmail.com>
2023-09-29 17:22:02 +0300

    6 commmit

--- src/main/java/study/java/springfirstlesson/service/CalculatorService.java
+++ src/main/java/study/java/springfirstlesson/service/CalculatorService.java
@@ -5,20 +5,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 @Service
 public class CalculatorService {
-    private int counter = 10;
+//    private int counter = 10;
     public String calculator(){
         return "Добро пожаловать в калькулятор";
     }
     public String getSum(@RequestParam String num1, @RequestParam String num2) {
-        return num1 + " "+ " + " + num2 + " = " + counter;
+        return (num1 + num2);
     }
     public String getMinus(@RequestParam String num1, @RequestParam String num2) {
-        return num1 + " "+ " - " + num2 + "  = 0";
+        return (num1 + num2);
     }
     public String getMultiply(@RequestParam String num1, @RequestParam String num2) {
-        return num1 + " "+ " * " + num2 + "  = 25";
+        return (num1 + num2);
     }
     public String getDivide(@RequestParam String num1, @RequestParam String num2) {
-        return num1 + " "+ " / " + num2 + "  = 1";
+        return (num1 + num2);
     }
 }
